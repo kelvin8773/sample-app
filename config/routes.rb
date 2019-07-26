bfc22d4607880fbc7cb51fdd1506cbb69cdbe97e
Kelvin Liang <kelvin9877@gmail.com>
2019-07-27 00:53:23 +0800

    Implment basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root 'static_pages#home'
   
   get '/help',    to: 'static_pages#help'
@@ -8,6 +9,10 @@ Rails.application.routes.draw do
   # get 'users/new'
   get '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
+
+  get '/login',  to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
   resources :users
 
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -8,7 +8,9 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
-    assert_select "a[href=?]", signup_path, count: 2
+    assert_select "a[href=?]", signup_path, count: 1
+    assert_select "a[href=?]", login_path, count: 1
+
 
     get contact_path
     assert_select "title", full_title("Contact")
