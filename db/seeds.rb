b5efce0619b782c256a52e3c3b0b22087633884b
Kelvin Liang <kelvin9877@gmail.com>
2019-08-05 18:16:38 +0800

    before add 300 microposts

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,4 +25,11 @@ User.create!( name:  "Example User",
                 activated: true, 
                 activated_at: Time.zone.now )
 
+
+users = User.order(:created_at).take(6)
+50.times do
+  content = Faker::Lorem.sentence(rand(3..12))
+  users.each { |user| user.microposts.create!(content: content) }
+end
+
 end
