da37444f300514f75181a2996e9f00f09ea70f44
Kelvin Liang <kelvin9877@gmail.com>
2019-08-05 18:42:16 +0800

    after add 300 posts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,10 +9,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    redirect_to root_url and return unless @user.activated?
-
+    # redirect_to root_url and return unless @user.activated?
     @microposts = @user.microposts.paginate(page: params[:page])
-    
   end
 
   def new
--- db/seeds.rb
+++ db/seeds.rb
@@ -24,12 +24,11 @@ User.create!( name:  "Example User",
                 password_confirmation: password,
                 activated: true, 
                 activated_at: Time.zone.now )
-
+end
 
 users = User.order(:created_at).take(6)
+
 50.times do
   content = Faker::Lorem.sentence(rand(3..12))
   users.each { |user| user.microposts.create!(content: content) }
-end
-
 end
