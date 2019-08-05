bf9806e47bb26d0913af826fdfd3378aecb316a5
Kelvin Liang <kelvin9877@gmail.com>
2019-08-06 00:47:07 +0800

    add picture upload restrictions

--- config/routes.rb
+++ config/routes.rb
@@ -21,5 +21,4 @@ Rails.application.routes.draw do
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
 
-
 end
--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -10,6 +10,8 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     log_in_as(@user)
     get root_path
     assert_select 'div.pagination'
+    assert_select 'input[type = file]'
+
     # Invalid submission
     assert_no_difference 'Micropost.count' do
       post microposts_path, params: { micropost: { content: "" } }
@@ -17,10 +19,13 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     assert_select 'div#error_explanation'
     # Valid submission
     content = "This micropost really ties the room together"
+    picture = fixture_file_upload('test/fixtures/rails.png', 'image/png')
     assert_difference 'Micropost.count', 1 do
-      post microposts_path, params: { micropost: { content: content } }
+      post microposts_path, params: { micropost: { content: content,
+                                                    picture: picture } }
     end
     assert_redirected_to root_url
+    
     follow_redirect!
     assert_match content, response.body
     # Delete post
