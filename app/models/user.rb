307cf4aab012afa826592e4075d406a75e15738f
Kelvin Liang <kelvin9877@gmail.com>
2019-09-04 23:15:34 +0800

    update correct relationships with user & following

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,13 @@ class User < ApplicationRecord
   has_many :active_relationships, class_name: "Relationship",
                                     foreign_key: "follower_id",
                                     dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+
+  has_many :passive_relationships, class_name: "Relationship",
+                                    foreign_key: "followed_id",
+                                    dependent: :destroy
+  # has_many :followers, through: :passive_relationships, source: :follower
+  has_many :followers, through: :passive_relationships
 
 
   attr_accessor :remember_token, :activation_token, :reset_token
@@ -70,6 +77,18 @@ class User < ApplicationRecord
 
   def feed
     Micropost.where("user_id = ? ", id)
+  end
+
+  def follow(other_user)
+    following << other_user
+  end
+
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
   end
 
   private
